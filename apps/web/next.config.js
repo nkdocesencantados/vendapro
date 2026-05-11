317d91e023ab605511025b0d0b1e2525a3d6105e
nkdocesencantados <nk.docesencantados@gmail.com>
2026-05-11 17:30:35 -0300

    fix: atualizar next.config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,8 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  images: { domains: ["localhost"] },
+  output: 'standalone',
+  images: { domains: ['localhost'] },
   env: { NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL },
+  webpack: (config) => { return config; },
 };
 module.exports = nextConfig;
