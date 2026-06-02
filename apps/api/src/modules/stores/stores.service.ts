23108f42ac25d4a540765806e0d31b1298b0df1b
nkdocesencantados <nk.docesencantados@gmail.com>
2026-06-02 14:52:46 -0300

    fix: importar StoreStatus no stores service

--- apps/api/src/modules/stores/stores.service.ts
+++ apps/api/src/modules/stores/stores.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+﻿import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Store } from './store.entity';
+import { Store, StoreStatus } from './store.entity';
 
 @Injectable()
 export class StoresService {
